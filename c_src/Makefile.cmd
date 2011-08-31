4e410d2a48a17b4e2a42e5f6af20996a16d2b2b6
Michael Santos <michael.santos@gmail.com>
2011-08-31 11:01:20 -0400

    Use a private CFLAGS var

--- c_src/Makefile.cmd
+++ c_src/Makefile.cmd
@@ -9,4 +9,4 @@ dirs:
 	-@mkdir -p $(CMD_DIR)
 
 compile:
-	$(CC) -g -Wall -o $(CMD_PATH) -L. procket_cmd.c -lancillary
+	$(CC) $(PROCKET_CFLAGS) -g -Wall -o $(CMD_PATH) -L. procket_cmd.c -lancillary
