3fbc15c849a0bf0a3226b6d8d49b2ecc556cd105
Michael Santos <michael.santos@gmail.com>
2013-05-03 09:18:54 -0400

    Copyright heartbeat, adjust version to semver

--- c_src/procket.h
+++ c_src/procket.h
@@ -1,5 +1,4 @@
-/*
- * Copyright (c) 2010, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2010-2013, Michael Santos <michael.santos@gmail.com>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
--- include/bpf.hrl
+++ include/bpf.hrl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2011, Michael Santos <michael.santos@gmail.com>
+%% Copyright (c) 2011-2013, Michael Santos <michael.santos@gmail.com>
 %% All rights reserved.
 %%
 %% Redistribution and use in source and binary forms, with or without
