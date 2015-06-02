e4ca4b90b5aa59de81c835fdd662e15b5b0276bd
Jared Morrow <jared@helium.co>
2015-06-02 08:11:11 -0600

    Remove unneeded else statements, will make ifdef's easier

--- c_src/sockopt.h
+++ c_src/sockopt.h
@@ -9,17 +9,17 @@ int level_lookup(char *level_name, int level_size, int *level_value)
             *level_value = SOL_SOCKET;
             return 1;
         }
-        else if (strncmp("IPPROTO_IP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_IP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_IP;
             return 1;
         }
-        else if (strncmp("IPPROTO_TP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_TP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_TP;
             return 1;
         }
-        else if (strncmp("IPPROTO_AH", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_AH", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_AH;
             return 1;
@@ -32,47 +32,47 @@ int level_lookup(char *level_name, int level_size, int *level_value)
             *level_value = IPPROTO_TCP;
             return 1;
         }
-        else if (strncmp("IPPROTO_EGP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_EGP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_EGP;
             return 1;
         }
-        else if (strncmp("IPPROTO_PUP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_PUP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_PUP;
             return 1;
         }
-        else if (strncmp("IPPROTO_UDP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_UDP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_UDP;
             return 1;
         }
-        else if (strncmp("IPPROTO_IDP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_IDP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_IDP;
             return 1;
         }
-        else if (strncmp("IPPROTO_GRE", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_GRE", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_GRE;
             return 1;
         }
-        else if (strncmp("IPPROTO_ESP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_ESP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_ESP;
             return 1;
         }
-        else if (strncmp("IPPROTO_MTP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_MTP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_MTP;
             return 1;
         }
-        else if (strncmp("IPPROTO_PIM", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_PIM", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_PIM;
             return 1;
         }
-        else if (strncmp("IPPROTO_RAW", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_RAW", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_RAW;
             return 1;
@@ -85,41 +85,41 @@ int level_lookup(char *level_name, int level_size, int *level_value)
             *level_value = IPPROTO_IPV6;
             return 1;
         }
-        else if (strncmp("IPPROTO_ICMP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_ICMP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_ICMP;
             return 1;
         }
-        else if (strncmp("IPPROTO_IGMP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_IGMP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_IGMP;
             return 1;
         }
-        else if (strncmp("IPPROTO_IPIP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_IPIP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_IPIP;
             return 1;
         }
 #ifdef IPPROTO_DCCP
-        else if (strncmp("IPPROTO_DCCP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_DCCP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_DCCP;
             return 1;
         }
 #endif
-        else if (strncmp("IPPROTO_RSVP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_RSVP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_RSVP;
             return 1;
         }
 #ifdef IPPROTO_COMP
-        else if (strncmp("IPPROTO_COMP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_COMP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_COMP;
             return 1;
         }
 #endif
-        else if (strncmp("IPPROTO_SCTP", level_name, level_size) == 0)
+        if (strncmp("IPPROTO_SCTP", level_name, level_size) == 0)
         {
             *level_value = IPPROTO_SCTP;
             return 1;
@@ -175,7 +175,7 @@ int optname_lookup(char *option_name, int option_size, int *option_value)
             *option_value = SO_DEBUG;
             return 1;
         }
-        else if (strncmp("SO_ERROR", option_name, option_size) == 0)
+        if (strncmp("SO_ERROR", option_name, option_size) == 0)
         {
             *option_value = SO_ERROR;
             return 1;
@@ -188,12 +188,12 @@ int optname_lookup(char *option_name, int option_size, int *option_value)
             *option_value = SO_SNDBUF;
             return 1;
         }
-        else if (strncmp("SO_RCVBUF", option_name, option_size) == 0)
+        if (strncmp("SO_RCVBUF", option_name, option_size) == 0)
         {
             *option_value = SO_RCVBUF;
             return 1;
         }
-        else if (strncmp("SO_LINGER", option_name, option_size) == 0)
+        if (strncmp("SO_LINGER", option_name, option_size) == 0)
         {
             *option_value = SO_LINGER;
             return 1;
@@ -214,40 +214,40 @@ int optname_lookup(char *option_name, int option_size, int *option_value)
         }
 #endif
 #ifdef SO_PEERCRED
-        else if (strncmp("SO_PEERCRED", option_name, option_size) == 0)
+        if (strncmp("SO_PEERCRED", option_name, option_size) == 0)
         {
             *option_value = SO_PEERCRED;
             return 1;
         }
 #endif
 #ifdef SO_RCVLOAWAT
-        else if (strncmp("SO_RCVLOWAT", option_name, option_size) == 0)
+        if (strncmp("SO_RCVLOWAT", option_name, option_size) == 0)
         {
             *option_value = SO_RCVLOWAT;
             return 1;
         }
 #endif
 #ifdef SO_SNDLOWAT
-        else if (strncmp("SO_SNDLOWAT", option_name, option_size) == 0)
+        if (strncmp("SO_SNDLOWAT", option_name, option_size) == 0)
         {
             *option_value = SO_SNDLOWAT;
             return 1;
         }
 #endif
-        else if (strncmp("SO_RCVTIMEO", option_name, option_size) == 0)
+        if (strncmp("SO_RCVTIMEO", option_name, option_size) == 0)
         {
             *option_value = SO_RCVTIMEO;
             return 1;
         }
 #ifdef SO_NO_CHECK
-        else if (strncmp("SO_NO_CHECK", option_name, option_size) == 0)
+        if (strncmp("SO_NO_CHECK", option_name, option_size) == 0)
         {
             *option_value = SO_NO_CHECK;
             return 1;
         }
 #endif
 #ifdef SO_PRIORITY
-        else if (strncmp("SO_PRIORITY", option_name, option_size) == 0)
+        if (strncmp("SO_PRIORITY", option_name, option_size) == 0)
         {
             *option_value = SO_PRIORITY;
             return 1;
@@ -261,35 +261,35 @@ int optname_lookup(char *option_name, int option_size, int *option_value)
             *option_value = SO_REUSEADDR;
             return 1;
         }
-        else if (strncmp("SO_DONTROUTE", option_name, option_size) == 0)
+        if (strncmp("SO_DONTROUTE", option_name, option_size) == 0)
         {
             *option_value = SO_DONTROUTE;
             return 1;
         }
-        else if (strncmp("SO_BROADCAST", option_name, option_size) == 0)
+        if (strncmp("SO_BROADCAST", option_name, option_size) == 0)
         {
             *option_value = SO_BROADCAST;
             return 1;
         }
-        else if (strncmp("SO_KEEPALIVE", option_name, option_size) == 0)
+        if (strncmp("SO_KEEPALIVE", option_name, option_size) == 0)
         {
             *option_value = SO_KEEPALIVE;
             return 1;
         }
-        else if (strncmp("SO_OOBINLINE", option_name, option_size) == 0)
+        if (strncmp("SO_OOBINLINE", option_name, option_size) == 0)
         {
             *option_value = SO_OOBINLINE;
             return 1;
         }
 #ifdef SO_BSDCOMPAT
-        else if (strncmp("SO_BSDCOMPAT", option_name, option_size) == 0)
+        if (strncmp("SO_BSDCOMPAT", option_name, option_size) == 0)
         {
             *option_value = SO_BSDCOMPAT;
             return 1;
         }
 #endif
 #ifdef SO_REUSEPORT
-        else if (strncmp("SO_REUSEPORT", option_name, option_size) == 0)
+        if (strncmp("SO_REUSEPORT", option_name, option_size) == 0)
         {
             *option_value = SO_REUSEPORT;
             return 1;
